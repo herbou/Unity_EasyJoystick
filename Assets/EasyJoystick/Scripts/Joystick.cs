e1aec4c5b27c2c14f315d4151f604c16c0d511f6
hamza herbou <hamza95herbou@gmail.com>
2021-06-16 15:26:09 +0100

    add joystick events

--- Assets/EasyJoystick/Scripts/Joystick.cs
+++ Assets/EasyJoystick/Scripts/Joystick.cs
@@ -16,13 +16,12 @@ namespace EasyJoystick {
 
    public class Joystick : MonoBehaviour,IPointerDownHandler,IDragHandler,IPointerUpHandler {
 
-      [SerializeField] private RectTransform container ;
-      [SerializeField] private RectTransform handle ;
+      private RectTransform container ;
+      private RectTransform handle ;
 
       /// <summary>
       /// Enable it if you want to use your keyboard's arrow keys as a joystick too.
       /// </summary>
-      [Space (20f)]
       [Tooltip ("enable it if you want to use your keyboard's arrow keys as a joystick too.")]
       public bool ArrowKeysSimulationEnabled = false ;
 
@@ -35,19 +34,29 @@ namespace EasyJoystick {
 
       public bool IsTouching { get { return _isTouching ; } }
 
+      public UnityAction OnJoystickDownAction ;
+      public UnityAction OnJoystickUpAction ;
+
       private PointerEventData pointerEventData ;
       private Camera cam ;
-      
-      
-      private void OnEnable() {
-        OnPointerUp (null);
+
+
+
+      private void OnEnable () {
+         OnPointerUp (null) ;
       }
 
       private void Awake () {
+         container = transform.GetComponent <RectTransform> () ;
+         handle = container.GetChild (0).GetComponent <RectTransform> () ;
+
          maxLength = (container.sizeDelta.x / 2f) - (handle.sizeDelta.x / 2f) - 5f ;
       }
 
       public void OnPointerDown (PointerEventData e) {
+         if (OnJoystickDownAction != null)
+            OnJoystickDownAction.Invoke () ;
+
          _isTouching = true ;
          cam = e.pressEventCamera ;
          OnDrag (e) ;
@@ -68,6 +77,9 @@ namespace EasyJoystick {
       }
 
       public void OnPointerUp (PointerEventData e) {
+         if (OnJoystickUpAction != null)
+            OnJoystickUpAction.Invoke () ;
+
          _isTouching = false ;
          normalizedPoint = Vector3.zero ;
          handle.anchoredPosition = Vector3.zero ;
